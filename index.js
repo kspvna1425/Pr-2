ef945ea749a77b165ec05f52c0312e50fc7e77d5
Vikash Raghav <104614541+kspvna1425@users.noreply.github.com>
2024-02-10 23:37:10 +0530

    pr-1
    ok complete

---
+++ index.js
@@ -0,0 +1,11 @@
+function Function(){
+    var x = document.getElementById("menu"); 
+     
+     if(x.style.display == "block")
+     {
+         x.style.display = "none";
+     }
+     else{
+         x.style.display = "block";
+     }
+ }
